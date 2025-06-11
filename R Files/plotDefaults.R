0f9af3733bacc4688e46adc1845f1f334ce2cb31
CallumGrape <callum06@gmail.com>
2025-06-11 12:23:11 +1000

    Moved plotDefaults to a separate script, so that it can be imported using source() from other files.

---
+++ R Files/plotDefaults.R
@@ -0,0 +1,32 @@
+## Default options for plots
+plotSun <- 0
+plotTide <- 0
+plotTidalCurve <- 1
+completeTimeRange <- 0
+
+sunriseColour = "orange"
+sunsetColour = "purple"
+sunLineAlpha = 0.6
+sunLineWidth = 1
+sunLineType = "longdash"
+sunFillAlpha = 0.06
+
+highTideColour = "blue"
+lowTideColour = "#61caff"
+#tideColour = "blue"
+tideColour = "#61caff"
+tideLineWidth = 0.5
+tideAlpha = 0.3
+
+vlineAlpha <- 0.3
+vlineWidth <- 1
+pointSize <- 1.2
+pointShape <- "circle"
+
+fontSizeAxisTitle = 14
+fontSizeAxisTicks = 10
+fontSizeLegendTitle = 13 
+fontSizeLegendLabels = 11
+paddingAxisTitle = 10
+
+rscript
