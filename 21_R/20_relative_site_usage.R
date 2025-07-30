658b6e5a6485deca8402c95a491f95a99c8371b3
Maxime Marini <marini.maxime@hotmail.fr>
2025-07-30 12:48:41 +1000

    changing 22_qmd to docs

--- 21_R/20_relative_site_usage.R
+++ 21_R/20_relative_site_usage.R
@@ -5,4 +5,3 @@
 ## Main   :  Access the relative site usage from birds in regard of the survey period for each MOTUS station 
 ## Created:  2025 July 
 
-source(here::here("21_R", "software_updates.R"), echo = FALSE)
