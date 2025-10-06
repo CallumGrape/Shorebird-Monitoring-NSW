1a326c33a060bf49791c2eb92bd014b4c80f96ec
Maxime Marini <c3541851@uon.edu.au>
2025-10-07 09:02:00 +1100

    Update

--- 2_R/30_motus_array_suitability_figure_2.R
+++ 2_R/30_motus_array_suitability_figure_2.R
@@ -2,7 +2,7 @@
 ## Type   :  PhD Project
 ## Auteur :  Maxime Marini
 ## Topic  :  Habitat selection from migratory shorebirds within and across Hunter & Port Stephen estuaries
-## Main   :  Access the efficiency of the local MOTUS array depending the monitored bird species
+## Main   :  Figure 2 - Access the efficiency of the local MOTUS array depending the monitored bird species
 ## Created:  2025 August
 
 
