9865793e8b8728a00d907c752f73b3d0e0f7d72a
Maxime Marini <c3541851@uon.edu.au>
2025-10-15 17:30:37 +1100

    update

---
+++ 2_R/21_relative_habitat_selection_figure_1.R
@@ -0,0 +1,73 @@
+
+## Type   :  PhD Project
+## Auteur :  Maxime Marini
+## Topic  :  Habitat selection from migratory shorebirds within and across Hunter & Port Stephen estuaries
+## Main   :  Available time vs Used time at night/day during high/low tide periods, from shorebirds 
+## Created:  2025 October 
+
+
+# 1 - Packages ----
+
+library(motus)
+library(dplyr)
+library(here)
+library(forcats) 
+library(ggplot2)
+library(lubridate)
+library(tidyr)
+library(purrr)
+
+# 2 - Settings ----
+
+# Set WD
+setwd(dirname(rstudioapi::getSourceEditorContext()$path)) 
+
+# 3 - Load data ----
+
+# Birds
+data_all <- readRDS(
+  tail(sort(list.files(
+    here::here("1_data", "alltags", "motus.rds"),
+    pattern = "-data\\.rds$", full.names = TRUE
+  )), 1)) 
+
+# Receivers info
+recv <- readRDS(
+  tail(sort(list.files(
+    here::here("1_data", "alltags", "motus.rds"),
+    pattern = "-recv-info\\.rds$", full.names = TRUE
+  )), 1)) 
+
+# 4 - Process data ----
+
+data_lt <- data_all %>%
+  filter(tideHighLow == "Low")
+
+data_ht <- data_all %>%
+  filter(tideHighLow == "High")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
